f4bff136378e7c7e8e805ed5fbbe105ebdd6cf49
tamura <sotaime+gh@gmail.com>
2024-02-19 15:54:07 +0900

    add rolinghash

---
+++ libs/string/src/lib.rs
@@ -0,0 +1,139 @@
+#[allow(non_snake_case)]
+/// rolling hash
+/// 以下記事の実装
+/// https://qiita.com/keymoon/items/11fac5627672a6d6a9f6
+pub mod rolling_hash {
+    const MASK30: u64 = (1u64 << 30) - 1;
+    const MASK31: u64 = (1u64 << 31) - 1;
+    const MOD: u64 = (1u64 << 61) - 1;
+    const MASK61: u64 = MOD;
+    const POSITIVIZER: u64 = MOD * 4;
+
+    /// rolling hashの基数
+    pub const B: u64 = 1_000_000_000 + 7;
+
+    /// mod 2^61-1を計算する関数
+    pub fn CalcMod(x: u64) -> u64 {
+        let xu = x >> 61;
+        let xd = x & MASK61;
+        let mut res = xu + xd;
+        if res >= MOD {
+            res -= MOD;
+        }
+        res
+    }
+
+    /// a*b mod 2^61-1を返す関数(最後にModを取らない)
+    pub fn Mul(a: u64, b: u64) -> u64 {
+        let au = a >> 31;
+        let ad = a & MASK31;
+        let bu = b >> 31;
+        let bd = b & MASK31;
+        let mid = ad * bu + au * bd;
+        let midu = mid >> 30;
+        let midd = mid & MASK30;
+        au * bu * 2 + midu + (midd << 31) + ad * bd
+    }
+    /// v^e mod 2^61-1 を計算します
+    pub fn CalcPow(v: u64, e: usize) -> u64 {
+        if e == 0 {
+            return 1;
+        }
+        let mut x = v;
+        let mut res = 1;
+        let mut i = 0;
+        loop {
+            if e >> i <= 0 {
+                break;
+            }
+            if e >> i & 1 == 1 {
+                res = CalcMod(Mul(res, x));
+            }
+            x = CalcMod(Mul(x, x));
+            i += 1;
+        }
+        res
+    }
+
+    /// 数列 s のハッシュを取得します
+    pub fn hash(s: &[u64]) -> u64 {
+        s.iter().fold(0u64, |s, &v| CalcMod(Mul(s, B) + v))
+    }
+
+    /// 末尾に `v` を追加したハッシュを取得します
+    /// + `h`: 現在の数列の hash
+    pub fn push_back(h: u64, v: u64) -> u64 {
+        CalcMod(Mul(h, B) + v)
+    }
+
+    /// 先頭に `v` を追加したハッシュを取得します
+    /// + `h`: 現在の数列の hash
+    /// + `current_len`: 現在の数列の 長さ
+    ///
+    /// 計算量: O(log(current_len))
+    pub fn push_front(h: u64, current_len: usize, v: u64) -> u64 {
+        let b = CalcPow(B, current_len);
+        CalcMod(h + Mul(v, b))
+    }
+
+    /// a は b に含まれているか? を返します。
+    /// O(|a|+|b|)
+    pub fn contains( a: &[u64], b: &[u64]) -> bool {
+        let al = a.len();
+        let bl = b.len();
+        if al > bl {
+            return false;
+        }
+        let ah = hash(&a);
+        let mut bh = hash(&b[0..a.len()]);
+        let fa = CalcPow(B, al);
+        for i in 0..bl {
+            if ah == bh {
+                return true;
+            }
+            if i + al >= bl {
+                break;
+            }
+            let d = POSITIVIZER - Mul(b[i], fa);
+
+            bh = CalcMod(CalcMod(Mul(bh, B) + b[i + al]) + d);
+        }
+        false
+    }
+
+    #[test]
+    fn test_rolling_hash() {
+        let a = [2, 100, 5005];
+        let ah = hash(&a);
+        {
+            let mut h = 0;
+            for &v in &a {
+                h = push_back(h, v);
+            }
+            assert_eq!(ah, h);
+        }
+        {
+            let mut h = 0;
+            for (i, &v) in a.iter().rev().enumerate() {
+                h = push_front(h, i, v);
+            }
+            assert_eq!(ah, h);
+        }
+    }
+
+    #[test]
+    fn test_rolling_hash_contains() {
+        let a = [2, 100, 5005];
+        let b_true1 = [1, 2, 2, 100, 5005];
+        let b_true2 = [2, 100, 5005, 1, 2];
+        let b_false1 = [2, 100, 5004];
+        let b_false2 = [1, 100, 5005];
+        let b_false3 = [2, 100];
+        assert!(contains(&a, &a));
+        assert!(contains(&a, &b_true1));
+        assert!(contains(&a, &b_true2));
+        assert!(!contains(&a, &b_false1));
+        assert!(!contains(&a, &b_false2));
+        assert!(!contains(&a, &b_false3));
+    }
+}
